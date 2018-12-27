c5fd4867675970ee3663106b4157e0b6e3b4e361
Stanislav Beresnev <stanislav.beresnev@ext.addisongloballtd.com>
2018-12-27 17:08:14 +0100

    apply basscss

--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,6 @@
 require('@angular/material/prebuilt-themes/indigo-pink.css');
 require('bootstrap/dist/css/bootstrap-reboot.css');
+require('basscss-flexbox/css/flexbox.css');
 
 import 'typeface-roboto';
 import 'webpack-material-design-icons';
