7421163921c5b7b464dfb37cb2689ba9b323b7fc
Stanislav Beresnev <stanislav.beresnev@ext.addisongloballtd.com>
2019-06-20 14:48:20 +0200

    add animation to the skill set

--- src/modules/employee/components/employee-overview/employee-overview.component.ts
+++ src/modules/employee/components/employee-overview/employee-overview.component.ts
@@ -1,4 +1,13 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {
+    trigger,
+    style,
+    animate,
+    transition,
+    query,
+    stagger,
+    animateChild,
+} from '@angular/animations';
 import {Employee} from '../../models';
 
 export const EXPERTISE_LIMIT = 10;
@@ -7,6 +16,25 @@ export const EXPERTISE_LIMIT = 10;
     selector: 'employee-overview',
     templateUrl: './employee-overview.component.html',
     styleUrls: ['./employee-overview.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [
+        trigger('skill', [
+            transition(':enter', [
+                style({ opacity: 0, transform: 'translateX(-10px)' }),
+                animate(
+                    '0.5s cubic-bezier(0.35, 0, 0.25, 1)',
+                    style({ opacity: 1, transform: 'translateX(0px)' }),
+                )
+            ]),
+        ]),
+        trigger('skillset', [
+            transition('* => *', [
+                query(':enter', [
+                    stagger(10, animateChild()),
+                ]),
+            ]),
+        ]),
+    ],
 })
 export class EmployeeOverviewComponent {
     @Input() employee:Employee = null;
