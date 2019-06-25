3cf75d06bf03050cbecabfaaed52243b0316d9f4
Stanislav Beresnev <stanislav.beresnev@ext.addisongloballtd.com>
2019-06-25 09:53:14 +0200

    employee state added to redux persist

--- src/app/meta-reducers.ts
+++ src/app/meta-reducers.ts
@@ -2,10 +2,11 @@ import {ActionReducer, MetaReducer} from '@ngrx/store';
 import {localStorageSync} from 'ngrx-store-localstorage';
 
 import {FeatureStateName as authStateKey} from 'src/modules/authentication';
+import {FeatureStateName as employeeStateKey} from 'src/modules/employee';
 
 export function localStorageSyncReducer(reducer:ActionReducer<any>):ActionReducer<any> {
     return localStorageSync({
-        keys: [authStateKey],
+        keys: [authStateKey, employeeStateKey],
         rehydrate: true,
     })(reducer);
 }
