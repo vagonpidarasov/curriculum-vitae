372259ccc2da499d9723912e1eb3554eee4f058f
Stanislav Beresnev <stanislav.beresnev@ext.addisongloballtd.com>
2019-09-03 10:58:36 +0200

    remove here.com api keys from the codebase

---
+++ src/repositories/http/here.com.config.ts
@@ -0,0 +1,5 @@
+export const config = {
+    reverseGeocodeApiUrl: 'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json',
+    appId: process.env.HERE_COM_APP_ID,
+    appCode: process.env.HERE_COM_APP_CODE,
+};
